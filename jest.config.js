efc881b10a7176b1c04ff428bdae4b43c8458e5e
Erik Roberts <erik@bugg.co>
2022-05-17 17:40:28 -0400

    Fix unlinted file

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@
  */
 
 module.exports = {
-     // Automatically clear mock calls, instances and results before every test
+    // Automatically clear mock calls, instances and results before every test
     clearMocks: true,
     // Indicates whether the coverage information should be collected while executing the test
     collectCoverage: true,
@@ -18,5 +18,5 @@ module.exports = {
     // Indicates which provider should be used to instrument code for coverage
     coverageProvider: "v8",
     // A preset that is used as a base for Jest's configuration
-    preset: "ts-jest"
+    preset: "ts-jest",
 };
